23b40bc6b3e74faf6685f1313e5ab277a79c859e
veys66 <veysdoganer@gmail.com>
2022-12-21 21:50:38 +0300

    day02_T3_GoogleSearch

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+public class T3_GoogleSearch {
+    public static void main(String[] args) {
+
+
+        //TC#3: Google search
+        //1- Open a chrome browser
+        //2- Go to: https://google.com
+        //3- Write “apple” in search box
+        //4- Click google search button
+        //5- Verify title:
+        //Expected: Title should start with “apple” word
+
+
+
+
+
+
+
+    }
+}

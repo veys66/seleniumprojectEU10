3537e255e4bef82a79864fadf685e112b011847e
veys66 <veysdoganer@gmail.com>
2022-12-31 21:27:38 +0300

    utilities

--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -1,4 +1,14 @@
 package com.cydeo.utilities;
 
-public class WebDriverFactory {
+    //TASK: NEW METHOD CREATION
+    //Method name: getDriver
+    //Static method
+    //Accepts String arg: browser Type
+    //-This arg will determine what type of browser is opened
+    //-if "chrome" passed --> it will open chrome browser
+    //-if "firefox" passed --> it will open firefox browser
+    //RETURN TYPE: "WebDriver"
+    public class WebDriverFactory {
+
+        public static
 }

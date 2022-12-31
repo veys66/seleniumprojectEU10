84b00ff5642f9575c115a29e5ba40a29a98b4006
veys66 <veysdoganer@gmail.com>
2022-12-31 21:33:05 +0300

    utilities

--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -1,6 +1,11 @@
 package com.cydeo.utilities;
 
-    //TASK: NEW METHOD CREATION
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+//TASK: NEW METHOD CREATION
     //Method name: getDriver
     //Static method
     //Accepts String arg: browser Type
@@ -10,5 +15,17 @@ package com.cydeo.utilities;
     //RETURN TYPE: "WebDriver"
     public class WebDriverFactory {
 
-        public static
+        public static WebDriver getDriver(String browserType) {
+            if (browserType.equalsIgnoreCase("chrome")) {
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            }else if(browserType.equalsIgnoreCase("firefox")){
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            }else{
+                System.out.println("Given browser type does not exist/or is not currently supported");\
+                System.out.println("Driver= null");
+                return null;
+            }
+        }
 }

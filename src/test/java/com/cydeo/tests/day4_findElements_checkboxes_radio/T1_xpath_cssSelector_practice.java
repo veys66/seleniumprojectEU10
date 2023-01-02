f21d16657fa560395b1c68936832ab934a9e77cd
veys66 <veysdoganer@gmail.com>
2023-01-02 22:47:23 +0300

    day3_cssSelector_xpath

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
@@ -33,15 +33,24 @@ public class T1_xpath_cssSelector_practice {
         //Locate header using cssSelector: locate parent element and move down to h2
         WebElement header_ex1= driver.findElement(By.cssSelector("div.example>h2"));
 
-        ////Locate header using xpath and using web elements text "Forgot Password"
+        //Locate header using xpath and using web elements text "Forgot Password"
+        //WebElement header_ex2= driver.findElement(By.cssSelector("//h2[.='Forgot Password']"));
         WebElement header_ex2= driver.findElement(By.cssSelector("//h2[text()='Forgot Password']"));
 
-
-
-
         //c. “E-mail” text
+        WebElement emailLabel= driver.findElement(By.xpath("//label[@for='email']"));
+
         //d. E-mail input box
+        WebElement inputBox_ex1= driver.findElement(By.xpath("//input[@name='email']"));
+
+        //Locate inputBox using xpath contains method
+
+        //tagName[contains(@attribute, 'value')]
+        WebElement inputBox_ex2= driver.findElement(By.xpath("//input[contains(@pattern, 'a-z')]"));
+
         //e. “Retrieve password” button
+        WebElement retrievePasswordBtn= driver.findElement(By.xpath("//button[@id='form_submit']"));
+
         //f. “Powered by Cydeo text
         //4. Verify all web elements are displayed.
 

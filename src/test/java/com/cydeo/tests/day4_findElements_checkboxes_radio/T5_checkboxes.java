9f6cdc8bde4cb098fe32701e02343814e64e1170
veys66 <veysdoganer@gmail.com>
2023-01-03 16:23:38 +0300

    day4_findElements_checkboxes_radio

---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_checkboxes.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day4_findElements_checkboxes_radio;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_checkboxes {
+    public static void main(String[] args) {
+        //1. Go to https://practice.cydeo.com/checkboxes
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/checkboxes");
+
+        //2. Confirm checkbox #1 is NOT selected by default
+        //3. Confirm checkbox #2 is SELECTED by default.
+        //4. Click checkbox #1 to select it.
+        //5. Click checkbox #2 to deselect it.
+        //6. Confirm checkbox #1 is SELECTED.
+        //7. Confirm checkbox #2 is NOT selected.
+
+
+
+
+
+
+    }
+}

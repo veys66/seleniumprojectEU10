335c287afc7cf9b24f4fc5605470f0288124e280
veys66 <veysdoganer@gmail.com>
2023-01-03 16:37:09 +0300

    day4_findElements_checkboxes_radio

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_checkboxes.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_checkboxes.java
@@ -20,10 +20,10 @@ public class T5_checkboxes {
         WebElement checkbox2=driver.findElement(By.xpath("//input[@name='checkbox2']"));
 
         //2. Confirm checkbox #1 is NOT selected by default
-        System.out.println("checkbox1.isSelected() = " + checkbox1.isSelected());
+        System.out.println("checkbox1.isSelected(), expecting false = " + checkbox1.isSelected());
 
         //3. Confirm checkbox #2 is SELECTED by default.
-        System.out.println("checkbox2.isSelected() = " + checkbox2.isSelected());
+        System.out.println("checkbox2.isSelected(), expecting true = " + checkbox2.isSelected());
 
         //4. Click checkbox #1 to select it.
         Thread.sleep(2000);
@@ -34,8 +34,10 @@ public class T5_checkboxes {
         checkbox2.click();
 
         //6. Confirm checkbox #1 is SELECTED.
-        //7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkbox1.isSelected(), expecting true = " + checkbox1.isSelected());
 
+        //7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkbox2.isSelected(), expecting false = " + checkbox2.isSelected());
 
 
 

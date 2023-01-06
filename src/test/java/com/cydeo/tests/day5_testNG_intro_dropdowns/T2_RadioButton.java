1fac0102c1a7f480e89f3d81d98228ce174e160c
veys66 <veysdoganer@gmail.com>
2023-01-06 15:17:27 +0300

    day5_testNG_intro_dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButton.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButton.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import java.util.concurrent.TimeUnit;
 
 public class T2_RadioButton {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //TC #2: Radiobutton handling
         //1. Open Chrome browser
         WebDriver driver= WebDriverFactory.getDriver("chrome");
@@ -21,10 +21,15 @@ public class T2_RadioButton {
         //3. Click to “Hockey” radio button
         WebElement hockeyRadioBtn= driver.findElement(By.xpath("//input[@id='hockey']"));
 
+        Thread.sleep(2000);
         hockeyRadioBtn.click();
 
         //4. Verify “Hockey” radio button is selected after clicking.
-
+        if(hockeyRadioBtn.isSelected()){
+            System.out.println("Button is selected. Verification PASSED!");
+        }else {
+            System.out.println("Button is not selected. Verification FAILED!!!");
+        }
 
 
 

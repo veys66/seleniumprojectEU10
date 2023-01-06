f6ea3474597183193a0ad3934accea9b329103be
veys66 <veysdoganer@gmail.com>
2023-01-06 15:00:58 +0300

    day5_testNG_intro_dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StateElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StateElementRefEx.java
@@ -1,12 +1,14 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
 public class T1_StateElementRefEx {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //TC #1: StaleElementReferenceException handling
         //1. Open Chrome browser
@@ -18,8 +20,19 @@ public class T1_StateElementRefEx {
         driver.get("https://practice.cydeo.com/add_remove_elements/");
 
         //3. Click to “Add Element” button
+        //button[@onclick='addElement()']
+        WebElement addElementBtn=driver.findElement(By.xpath("//button[.='Add Element']"));
+
+        Thread.sleep(2000);
+        addElementBtn.click();
+
         //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteButton= driver.findElement(By.xpath("//button[@class='added-manually']"));
+
+        System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+
         //5. Click to “Delete” button.
+
         //6. Verify “Delete” button is NOT displayed after clicking.
         //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
 

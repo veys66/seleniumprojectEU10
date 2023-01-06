da17834d031c6be0303999d937231a1f3193873e
veys66 <veysdoganer@gmail.com>
2023-01-06 15:36:45 +0300

    day5_testNG_intro_dropdowns

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
@@ -0,0 +1,40 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class T3_RadioButton_cont {
+    public static void main(String[] args) {
+
+        //TC #2: Radiobutton handling
+        //1. Open Chrome browser
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to https://practice.cydeo.com/radio_buttons
+        driver.get("https://practice.cydeo.com/radio_buttons ");
+
+        //Locate name='sports' radio buttons and store them in a List of Web Element
+        List<WebElement> sportRadioButtons= driver.findElements(By.name("sport"));
+
+        //Loop through the List of WebElement and select matching result "hockey"
+        for(WebElement each : sportRadioButtons){
+            String eachId= each.getAttribute("id");
+            System.out.println("eachId = " + eachId);
+
+            if(eachId.equals("hockey")){
+
+                each.click();
+                System.out.println("Hockey is selected: " + each.isSelected());
+                break;
+
+            }
+        }
+    }
+}

0277de705a0574537c936160180db9772af79ac1
veys66 <veysdoganer@gmail.com>
2023-01-07 15:37:51 +0300

    tests.day5_testNG_intro_dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,10 +1,18 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
+
+    @BeforeClass
+    public void setupMethod(){
+            System.out.println("---> BeforeClass is running");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("---> AfterClass is running");
+    }
 
     @BeforeMethod
     public void setUpMethod(){
@@ -16,12 +24,12 @@ public class TestNG_Intro {
         System.out.println("---> AfterMethod is running");
     }
 
-    @Test(priority = 2)
+    @Test(priority = 1)
     public void test1(){
         System.out.println("Test 1 is running...");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     public void test2(){
         System.out.println("Test2 is running...");
     }

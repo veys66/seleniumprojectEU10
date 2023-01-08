99706c65f3a560514baa84cc1d1e92cf974108f7
veys66 <veysdoganer@gmail.com>
2023-01-08 16:39:23 +0300

    day6_alerts_iframes_windows

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -13,7 +13,8 @@ import java.util.concurrent.TimeUnit;
 public class T4_IframePractice {
     WebDriver driver;
 
-    @BeforeMethod
+    //@BeforeMethod
+    @BeforeClass
     public void setupMethod(){
         //1. Open browser
         driver= WebDriverFactory.getDriver("chrome");
@@ -25,6 +26,7 @@ public class T4_IframePractice {
 
     }
 
+    //@Ignore
     @Test
     public void iframe_test(){
 
@@ -35,6 +37,15 @@ public class T4_IframePractice {
         WebElement yourContentGoesHereText= driver.findElement(By.xpath("//p"));
 
         Assert.assertTrue(yourContentGoesHereText.isDisplayed());
+
+        //Verify “An iFrame containing the TinyMCE WYSIWYG Editor”
+        //To able to verify the header, we must switch back to "main HTML"
+        driver.switchTo().parentFrame();
+
+        WebElement headerText= driver.findElement(By.xpath("//h3"));
+
+        //assertion of header text is displayed on page
+        Assert.assertTrue(headerText.isDisplayed());
 
 
 

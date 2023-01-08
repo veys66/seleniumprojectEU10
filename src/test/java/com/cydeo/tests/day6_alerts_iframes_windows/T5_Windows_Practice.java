1fafadfd1b4dd3a886003293790dd10784cf8f2d
veys66 <veysdoganer@gmail.com>
2023-01-08 17:05:19 +0300

    day6_alerts_iframes_windows

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -30,6 +33,18 @@ public class T5_Windows_Practice {
         String mainHandle=driver.getWindowHandle();
 
         System.out.println("mainHandle = " + mainHandle);
+
+        //4. Assert: Title is “Windows”
+        String expectedTitle="Windows";
+        String actualTitle=driver.getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        //5. Click to: “Click Here” link
+        WebElement clickHereLink= driver.findElement(By.linkText("Click Here"));
+
+        //6. Switch to new Window.
+        //7. Assert: Title is “New Window”
 
 
     }

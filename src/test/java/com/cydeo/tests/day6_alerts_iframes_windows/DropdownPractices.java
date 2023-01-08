7b816e567e803881bce258431f47ae08f98b812e
veys66 <veysdoganer@gmail.com>
2023-01-08 14:27:52 +0300

    day6_alerts_iframes_windows

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -4,11 +4,22 @@ import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
+
+    public WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cybertekschool.com/dropdown ");
+    }
     @Test
     public void dropdown_task5() throws InterruptedException {
         //1. Open Chrome browser
@@ -16,7 +27,7 @@ public class DropdownPractices {
         WebDriver driver= WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://practice.cybertekschool.com/dropdown");
+        driver.get("https://practice.cybertekschool.com/dropdown ");
 
         // we located the dropdown and it is ready to use
         Select stateDropdown= new Select(driver.findElement(By.xpath("//select[@id='state']")));
@@ -27,12 +38,32 @@ public class DropdownPractices {
 
         //4. Select Virginia
         Thread.sleep(1000);
-        stateDropdown.deselectByValue("VA");
+        stateDropdown.selectByValue("VA");
 
         //5. Select California
+        Thread.sleep(1000);
+        stateDropdown.selectByIndex(5);
 
         //6. Verify final selected option is California.
         //Use all Select options. (visible text, value, index)
+
+        String expectedOptionText="California";
+        String actualOptionText= stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualOptionText, expectedOptionText);
+    }
+
+    @Test
+    public void dropdown_task6(){
+
+        //Select “December 1st, 1923” and verify it is selected.
+        Select yearDropdown= new Select(driver.findElement(By.xpath("")));
+        Select monthDropdown= new Select(driver.findElement(By.xpath("")));
+        Select dayDropdown= new Select(driver.findElement(By.xpath("")));
+        //Select year using  : visible text
+        //Select month using   : value attribute
+        //Select day using : index number
+
     }
 
 }

feab2ed40a708479eab905c2897535b1463d176f
veys66 <veysdoganer@gmail.com>
2023-01-11 22:41:17 +0300

    day7_webtables_utilities_javafaker

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRN_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRN_LOGIN.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,10 +29,25 @@ public class T3_CRN_LOGIN {
         driver.get("https://login1.nextbasecrm.com/");
 
         //3. Enter valid username
+        WebElement inputUsername= driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com ");
+
+        //helpdesk1@cybertekschool.com UserUser
+        //helpdesk2@cybertekschool.com UserUser
+
+
         //4. Enter valid password
+        WebElement inputPassword= driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
         //5. Click to `Log In` button
+        WebElement loginButton= driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+
         //6. Verify title is as expected:
         //Expected: Portal
+        BrowserUtils.verifyTitle(driver, "Portal");
 
     }
 }

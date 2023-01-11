2c371ba6b6a416d6757d094ab5a946261f7b3b0e
veys66 <veysdoganer@gmail.com>
2023-01-11 22:52:58 +0300

    day7_webtables_utilities_javafaker

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,6 +1,7 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
 import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.CRM_Utilities;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -43,6 +44,23 @@ public class T3_CRM_LOGIN {
         //5. Click to `Log In` button
         WebElement loginButton= driver.findElement(By.xpath("//input[@value='Log In']"));
         loginButton.click();
+
+
+        //6. Verify title is as expected:
+        //Expected: Portal
+        BrowserUtils.verifyTitle(driver, "Portal");
+
+    }
+
+
+    @Test
+    public void crm_login_test2(){
+
+        //2. Go to : https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+       //Calling my utility method to login helpdesk1
+        CRM_Utilities.crm_login(driver);
 
 
         //6. Verify title is as expected:
---
+++ src/test/java/com/cydeo/utilities/CRM_Utilities.java
@@ -0,0 +1,31 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CRM_Utilities {
+
+    /*
+    This method will log in with helpdesk1@cybertekschool.com user when it is called
+     */
+    public static void crm_login(WebDriver driver){
+        //3. Enter valid username
+        WebElement inputUsername= driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com ");
+
+        //helpdesk1@cybertekschool.com UserUser
+        //helpdesk2@cybertekschool.com UserUser
+
+
+        //4. Enter valid password
+        WebElement inputPassword= driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
+        //5. Click to `Log In` button
+        WebElement loginButton= driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+    }
+
+}

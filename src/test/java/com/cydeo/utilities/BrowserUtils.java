83fdc66c9b8b6e14f3b6a0f5bc3dbad3ed363b13
veys66 <veysdoganer@gmail.com>
2023-01-11 21:39:14 +0300

    day7_webtables_utilities_javafaker

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,26 @@
+package com.cydeo.utilities;
+
+/*
+In this class only general utility methods are not related to some specific page.
+ */
+
+public class BrowserUtils {
+    /*
+    This method will accept int (in seconds) and execute Thread.sleep fot given duration
+     */
+    public static void sleep(int second){
+        second *=1000;
+        try {
+            Thread.sleep(second);
+        }catch (InterruptedException e){
+
+        }
+    }
+
+
+
+
+
+
+
+}

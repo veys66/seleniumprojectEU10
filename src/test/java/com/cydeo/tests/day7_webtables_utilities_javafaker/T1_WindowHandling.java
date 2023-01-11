76dac93d2fda08e9764e9ee399e31e80134295dd
veys66 <veysdoganer@gmail.com>
2023-01-11 19:43:11 +0300

    day7_webtables_utilities_javafaker

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,8 +1,12 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
 
@@ -10,19 +14,25 @@ public class T1_WindowHandling {
 
     @BeforeMethod
     public void setupMethod(){
-
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
     public void window_handling_test(){
         //2. Go to : https://www.amazon.com
+        driver.get("https://www.amazon.com");
+
         //3. Copy-past the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+
         //4. Create a logic to switch to the tab where Etsy.com is open
         //5. Assert: Title contains “Etsy”
 
         //Lines to be pasted:
-        //((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
-        //((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
-        //((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+
     }
 }

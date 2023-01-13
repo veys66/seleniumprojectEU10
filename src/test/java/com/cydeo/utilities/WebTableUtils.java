2af30cd82a472ffc6370d3acd453ea917656df8d
veys66 <veysdoganer@gmail.com>
2023-01-13 14:25:06 +0300

    day8_properties_config_reader

--- src/test/java/com/cydeo/utilities/WebTableUtils.java
+++ src/test/java/com/cydeo/utilities/WebTableUtils.java
@@ -3,6 +3,7 @@ package com.cydeo.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class WebTableUtils {
 
@@ -27,14 +28,27 @@ public class WebTableUtils {
       // return driver.findElement(By.xpath("//td[.='"+customerName+"']/following-sibling::td[3]")).getText();
     //}
 
+
+
     //Method #2 info:
-            //• Name: orderVerify ()
-//• Return type: void
-//• Arg1: WebDriver driver
-//• Arg2: String costumerName
-//• Arg3: String expectedOrderDate
-    //This method should accept above mentioned arguments and internally assert
+    //• Name: orderVerify ()
+    //• Return type: void
+    //• Arg1: WebDriver driver
+    //• Arg2: String costumerName
+    //• Arg3: String expectedOrderDate
+    //This method should accept above-mentioned arguments and internally assert
     //expectedOrderDate matching actualOrderDate.
+
+    public static void orderVerify(WebDriver driver, String customerName, String expectedOrderDate) {
+        String locator="//td[.='"+customerName+"']/following-sibling::td[3]";
+        WebElement customerDateCell=driver.findElement(By.xpath(locator));
+
+        String actualOrderDate=customerDateCell.getText();
+
+        Assert.assertEquals(actualOrderDate, expectedOrderDate);
+    }
+
+
 
 
 }

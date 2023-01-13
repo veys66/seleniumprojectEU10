014534c376afbec8c9402933e490401a2990c6ff
veys66 <veysdoganer@gmail.com>
2023-01-13 16:21:00 +0300

    day8_properties_config_reader

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
@@ -0,0 +1,5 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+public class T4_Config_Practice {
+
+}
--- src/test/java/com/cydeo/utilities/ConfigurationReader.java
+++ src/test/java/com/cydeo/utilities/ConfigurationReader.java
@@ -26,4 +26,11 @@ public class ConfigurationReader {
        }
    }
 
+   public static String getProperty(String keyword){
+        return properties.getProperty(keyword);
+   }
+
+
+
+
 }

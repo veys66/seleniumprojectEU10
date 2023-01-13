4aeb7b94dd46d292f533dafa6730927f62122937
veys66 <veysdoganer@gmail.com>
2023-01-13 15:35:36 +0300

    day8_properties_config_reader

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
@@ -6,6 +6,10 @@ public class LearningProperties {
 
     @Test
     public void java_properties_reading_test(){
+        //key=value
+        //os.name= Mac OS X
+        //user.name= user
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
     }
 }

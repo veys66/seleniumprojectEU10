5e29f28a78b7d09994f2d2d6a99f519edc7b719c
veys66 <veysdoganer@gmail.com>
2023-01-20 22:34:51 +0300

    day10_upload_actions_jsexecutor

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
@@ -37,13 +37,19 @@ public class T1_Registration_Form {
 
         inputUserName.sendKeys(user);
 
-
-
         //6. Enter email address
         WebElement inputEmail=Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
+        inputEmail.sendKeys(user+ "@email.com");
 
         //7. Enter password
+        WebElement inputPassword=Driver.getDriver().findElement(By.xpath("//input[@name='password']"));
+        inputPassword.sendKeys(faker.numerify("########"));
+        //System.out.println("faker.internet().password() = " + faker.internet().password());
+
         //8. Enter phone number
+        WebElement inputPhone=Driver.getDriver().findElement(By.xpath("//input[@name='phone']"));
+        inputPhone.sendKeys(faker.numerify("###-###-####"));
+
         //9. Select a gender from radio buttons
         //10. Enter date of birth
         //11. Select Department/Office

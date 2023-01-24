43f15ac0b432ace55325a22cc5220562e4300509
veys66 <veysdoganer@gmail.com>
2023-01-24 13:55:44 +0300

    day11_actions_jsexecutor_practice

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -32,11 +32,15 @@ public class ActionsPractices {
         //5- Scroll back up to “Home” link using PageUP button
         actions.sendKeys(Keys.PAGE_UP, Keys.PAGE_UP).perform();
 
-
+        Driver.getDriver().close();
 
 
     }
 
+    @Test
+    public void test2(){
+        Driver.getDriver().get("https://practice.cydeo.com/");
+    }
 
 
 }

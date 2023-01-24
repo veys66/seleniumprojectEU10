7bd942d061bbb5620f34772ce7ecc234de4a48f9
veys66 <veysdoganer@gmail.com>
2023-01-24 13:02:14 +0300

    day10_upload_actions_jsexecutor

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
@@ -18,15 +18,18 @@ public class T6_JavascriptExecutor {
         //3- Use below JavaScript method and scroll
         JavascriptExecutor js= (JavascriptExecutor) Driver.getDriver();
 
+        //a.  750 pixels down 10 times.
         for (int i = 0; i < 10; i++) {
             BrowserUtils.sleep(1);
             js.executeScript("window.scrollBy(0,750)");
         }
 
-
-        //a.  750 pixels down 10 times.
         //b.  750 pixels up 10 times
-        //JavaScript method to use : window.scrollBy(0,0)
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,-750)");
+        }
+        //JavaScript's method to use : window.scrollBy(0,0)
 
 
     }

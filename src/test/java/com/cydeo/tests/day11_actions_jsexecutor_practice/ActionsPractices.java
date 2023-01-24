29f11b54f5c6d7132f6716beb77f08edb450002b
veys66 <veysdoganer@gmail.com>
2023-01-24 13:40:32 +0300

    day11_actions_jsexecutor_practice

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -1,6 +1,9 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ActionsPractices {
@@ -13,8 +16,17 @@ public class ActionsPractices {
         Driver.getDriver().get("https://practice.cydeo.com/");
 
         //3- Scroll down to “Powered by CYDEO”
+        //Create object of Actions and pass our "driver" instance
+        Actions actions=new Actions(Driver.getDriver());
+
+        //Locating cydeo link to be able to pass in the actions method
+        //a[@href='https://practice.cydeo.com/'
+        //div)[10]
+
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
 
         //4- Scroll using Actions class “moveTo(element)” method
+
     }
 
 

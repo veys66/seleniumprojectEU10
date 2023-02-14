d1cd250192e2c48803bde049d0a0738c1fa35cd2
veys66 <veysdoganer@gmail.com>
2023-02-14 19:35:04 +0300

    day12_pom_design_explicit_wait

---
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day12_pom_design_explicit_wait;
+
+import com.cydeo.pages.LibraryLoginPage;
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class POMPractices {
+
+
+    @Test
+    public void required_field_error_message_test(){
+        //TC #1: Required field error message test
+        //1- Open a Chrome browser
+        //2- Go to: https://library1.cydeo.com
+        Driver.getDriver().get("https://library1.cydeo.com");
+
+        //3- Do not enter any information
+        //4- Click to “Sign in” button
+        LibraryLoginPage libraryLoginPage=new LibraryLoginPage();
+
+
+        //5- Verify expected error is displayed:
+        //Expected: This field is required.
+
+
+    }
+
+    }

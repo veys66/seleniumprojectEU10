3b29d777bff0384d2714ed8ab5e6fad3d37e5f99
veys66 <veysdoganer@gmail.com>
2023-02-15 11:26:18 +0300

    day12_pom_design_explicit_wait

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
@@ -31,8 +31,21 @@ public class WebDriveWaitPractices {
 
         //4. Assert: Image is displayed.
         Assert.assertTrue(dynamicLoad7Page.spongeBobImage.isDisplayed());
+    }
 
+    @Test
+    public void dynamic_load_1_test(){
+        //1. Go to https://practice.cydeo.com/dynamic_loading/1
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
 
+        //2. Click to start
+
+        //3. Wait until loading bar disappears
+        //4. Assert username inputbox is displayed
+        //5. Enter username: tomsmith
+        //6. Enter password: incorrectpassword
+        //7. Click to Submit button
+        //8. Assert “Your password is invalid!” text is displayed.
     }
 
 

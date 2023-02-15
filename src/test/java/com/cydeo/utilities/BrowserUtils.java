eea115acf3aeaa5a40ecf7c1474c9b116952aa7e
veys66 <veysdoganer@gmail.com>
2023-02-15 21:33:14 +0300

    utilities

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -5,9 +5,13 @@ In this class only general utility methods are not related to some specific page
  */
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class BrowserUtils {
     /*
@@ -54,10 +58,18 @@ public class BrowserUtils {
 This method accepts a String "expectedTitle" and Asserts if it is true
  */
 
-public static void verifyTitle(WebDriver driver, String expectedTitle){
+    public static void verifyTitle(WebDriver driver, String expectedTitle){
     Assert.assertEquals(driver.getTitle(), expectedTitle);
 }
 
+    /*
+    Creating a utility method for ExplicitWait, so we don't have to repeat the lines
+     */
+    public static void waitForInvisibilityOf(WebElement webElement){
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait= new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
 
 
 }

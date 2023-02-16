052f10ee810c38f177dc04416687635544dff6c2
veys66 <veysdoganer@gmail.com>
2023-02-16 20:16:51 +0300

    page

--- src/test/java/com/cydeo/pages/DynamicControlsPage.java
+++ src/test/java/com/cydeo/pages/DynamicControlsPage.java
@@ -25,6 +25,11 @@ public class DynamicControlsPage {
     @FindBy(css= "p[id='message']")
     public WebElement message;
 
+    @FindBy(xpath ="//button[.='Enable']")
+    public WebElement enableButton;
+
+    @FindBy(css ="input[type='text']")
+    public WebElement inputBox;
 
 
 
